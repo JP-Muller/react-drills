7b05cf2750541070a6f0156450c8a481b3950c19
JP-Muller <mullerjp95@gmail.com>
2019-06-18 19:28:43 -0600

    react2AP progress

---
+++ app-5/src/Image.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Image extends Component {
+ render(){
+
+        return(
+        <div>
+        <img src={this.props.url} alt="Rochester" />
+        <caption>Error 599</caption>
+
+
+        </div>
+
+        )
+    }
+}
+
+export default Image
